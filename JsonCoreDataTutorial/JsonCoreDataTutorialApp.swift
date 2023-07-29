33fafe20322deaea44946ee7ed8b10248e9be1ad
codelab21 <codelab21@outlook.com>
2023-07-29 14:28:54 +0300

    Initial Commit

---
+++ JsonCoreDataTutorial/JsonCoreDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonCoreDataTutorialApp.swift
+//  JsonCoreDataTutorial
+//
+//  Created by Eymen on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct JsonCoreDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
